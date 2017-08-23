10e7149506abeb154dc39530fabb30440c18a1cd
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2017-08-23 19:27:07 +0530

    delete chats because it already exists and it does not matter

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 20170813110306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "chats", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "visitor_id"
-    t.string "session_var"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
