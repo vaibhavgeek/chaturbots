3f86d2adbb87213029967f6f0a6cd08b0efa48e8
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2017-11-10 16:39:25 +0530

    allow nginx to access assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,14 +3,14 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-config.action_dispatch.default_headers = { 'X-Frame-Options' => 'ALLOWALL' }
+  config.action_dispatch.default_headers = { 'X-Frame-Options' => 'ALLOWALL' }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-config.serve_static_files = false
+  config.serve_static_assets = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -24,13 +24,12 @@ config.serve_static_files = false
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = true
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
