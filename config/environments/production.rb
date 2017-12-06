275c1698897e2c522b4e5bb528b8c0577089ad41
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2017-12-06 17:09:46 +0530

    commit to github, production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'ws://139.59.63.120/cable'
-  config.action_cable.allowed_request_origins = [ 'http://139.59.63.120' ]
+  config.action_cable.allowed_request_origins = [ 'http://139.59.63.120' , 'http://www.chaturbots.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
