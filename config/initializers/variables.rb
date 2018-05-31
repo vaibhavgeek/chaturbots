cac8a611d5f1b5a32187b142c2f16365c57fa54d
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2018-05-31 17:59:55 +0530

    change url

--- config/initializers/variables.rb
+++ config/initializers/variables.rb
@@ -1,9 +1,11 @@
 
 if Rails.env.production?
 	ENV['LINK_MAIN'] = 'http://www.chaturbots.com'
-	ENV['LINK_PYTHON'] = 'http://api.chaturbots.com'
+	ENV['LINK_PYTHON'] = 'http://35.196.230.214'
 else
 	ENV['LINK_MAIN'] = 'http://localhost:3000'
 	#ENV['LINK_PYTHON'] = 'http://api.chaturbots.com'
 	ENV['LINK_PYTHON'] = 'http://0.0.0.0:33507'
 end
+
+
