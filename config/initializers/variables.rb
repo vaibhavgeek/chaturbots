9dc4bcccaa6fafec621179ed61557fb52a0d4a4b
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2018-05-31 18:32:45 +0530

    remove read more for every message

--- config/initializers/variables.rb
+++ config/initializers/variables.rb
@@ -4,8 +4,7 @@ if Rails.env.production?
 	ENV['LINK_PYTHON'] = 'http://35.196.230.214'
 else
 	ENV['LINK_MAIN'] = 'http://localhost:3000'
-	#ENV['LINK_PYTHON'] = 'http://api.chaturbots.com'
-	ENV['LINK_PYTHON'] = 'http://0.0.0.0:33507'
+	ENV['LINK_PYTHON'] = 'http://35.196.230.214'
 end
 
 
