fdae860a255898b02136dcbc3af468af1e2cee2a
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2018-06-21 17:45:59 +0530

    actioncable ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'ws://chaturbots.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://139.59.63.120' , 'http://www.chaturbots.com' ]
+  config.action_cable.url = 'wss://chaturbots.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://139.59.63.120' , 'https://chaturbots.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
