72e3efaef48729756c44b43b89d4c7e038979a73
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2018-06-25 23:16:37 +0530

    change variables

--- config/initializers/variables.rb
+++ config/initializers/variables.rb
@@ -1,6 +1,6 @@
 
 if Rails.env.production?
-	ENV['LINK_MAIN'] = 'http://www.chaturbots.com'
+	ENV['LINK_MAIN'] = 'https://chaturbots.com'
 	ENV['LINK_PYTHON'] = 'http://35.196.230.214'
 else
 	ENV['LINK_MAIN'] = 'http://localhost:3000'
